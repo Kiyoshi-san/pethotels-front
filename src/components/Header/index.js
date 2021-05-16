625e7e98d6cf7a58aba225b1e7f475e713a4e761
Kiyoshi <kiyoshibudokan@hotmail.com>
2021-05-15 21:58:14 -0300

    Created the style category menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Search from "../Search";
 import { Link } from "react-router-dom";
 import MenuCategory from "../Menu/MenuCategory";
-import { useDispatch, useSelector } from "react-redux";
-import { listCategories } from "../../redux/actions/category";
 
 export default function Header() {
-  const categoryList = useSelector((state) => state.categoriesList);
-  const { categories } = categoryList;
-
-  const dispatch = useDispatch(listCategories());
-  useEffect(() => {
-    dispatch(listCategories());
-    console.log("aaa");
-    console.log(categories);
-    console.log("aaa");
-  }, []);
-
   return (
     <div className="top-header-menu-container">
-      <MenuCategory categories={categories} />
-      <div className="logo">
-        <Link to={"/"}>
-          <i className="icon icon-logo"></i>
-        </Link>
-      </div>
-      <Search />
-      <div className="cta-icons">
-        <i className="icon icon-heart-empty hide-for-small-only"></i>
-        <div className="top-header-menu--my-account">
-          <span className="hide-for-small-only">Minha conta</span>
-          <i className="icon icon-profile show-for-small-only"></i>
+      <div className="top-header-menu-box">
+        <div className="logo">
+          <Link to={"/"}>
+            <i className="icon icon-logo"></i>
+          </Link>
         </div>
-        <i className="icon icon-cart"></i>
+        <Search />
+        <div className="cta-icons">
+          <i className="icon icon-heart-empty hide-for-small-only"></i>
+          <div className="top-header-menu--my-account">
+            <span className="hide-for-small-only">Minha conta</span>
+            <i className="icon icon-profile show-for-small-only"></i>
+          </div>
+          <i className="icon icon-cart"></i>
+        </div>
       </div>
+      <MenuCategory />
     </div>
   );
 }
--- src/components/Menu/MenuCategory/index.js
+++ src/components/Menu/MenuCategory/index.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { listCategories } from "../../../redux/actions/category";
 
-export default function MenuCategory(props) {
-  const { categories } = props;
+export default function MenuCategory() {
+  const categoryList = useSelector((state) => state.categoriesList);
+  const { categories } = categoryList;
+  const [categoryPosFixed, setCategoryPosFixed] = useState("");
+
+  const categoriesRoot = categories?.filter((obj) => {
+    return obj.parentCategoryId === "root";
+  });
+
+  const dispatch = useDispatch(listCategories());
+  useEffect(() => {
+    dispatch(listCategories());
+    console.log("aaa");
+    console.log(categoriesRoot);
+    console.log("aaa");
+  }, []);
+
+  /* const headerHeight = document.querySelectorAll(
+    ".top-header-menu-box"
+  ).offsetTop; */
+  const headerHeight = 90;
+
+  useEffect(() => {
+    window.addEventListener("scroll", (a) => {
+      if (window.scrollY >= headerHeight) {
+        setCategoryPosFixed("category-fixed");
+      } else {
+        setCategoryPosFixed("");
+      }
+    });
+  });
 
   return (
-    <div className="menu-category-container">
-      <i className="icon icon-menu show-for-small-only"></i>
-      {categories?.map((cat) => (
-        <div key="cat.name">{cat.name}</div>
-      ))}
+    <div className={`menu-category-container ${categoryPosFixed}`}>
+      <div className="menu-category-box">
+        <i className="icon icon-menu show-for-small-only"></i>
+        {categoriesRoot?.map((cat) => (
+          <div className="category-item" key={`${cat.name}${cat.categoryId}`}>
+            <Link to={`${cat.categoryId}`}>
+              <span>{cat.name}</span>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
