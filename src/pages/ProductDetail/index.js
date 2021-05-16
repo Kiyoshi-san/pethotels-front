526d1fbbc527db54d07c509195458f545235399d
Kiyoshi <kiyoshibudokan@hotmail.com>
2021-05-16 15:06:32 -0300

    Removed the react-slick it was not being compatible to react-image-magnifiers, and installed react multi carousel because this has thumbnails

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,44 +1,14 @@
-import React, { useState, useEffect } from "react";
-
-// https://www.freakyjolly.com/react-slick-carousel-with-custom-navigation-and-lazy-loaded-images-in-slider/
-// https://react-slick.neostack.com/docs/example/as-nav-for
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-// https://github.com/adamrisberg/react-image-magnifiers.git
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { SideBySideMagnifier } from "react-image-magnifiers";
+import { Carousel } from "react-responsive-carousel";
+import img1 from "../../assets/images/banner/banner1.jpg";
+import img2 from "../../assets/images/banner/banner2.jpg";
+import img3 from "../../assets/images/banner/banner3.jpg";
+import img4 from "../../assets/images/banner/banner4.jpg";
 
-export default function ProductDetail(props) {
-  const [nav1, setNav1] = useState(null);
-  const [nav2, setNav2] = useState(null);
-  const [slider1, setSlider1] = useState(null);
-  const [slider2, setSlider2] = useState(null);
-
-  useEffect(() => {
-    setNav1(slider1);
-    setNav2(slider2);
-  });
-
-  const settingsMain = {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    arrows: false,
-  };
-
-  const settingsThumbs = {
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: ".slider-for",
-    dots: false,
-    centerMode: true,
-    swipeToSlide: true,
-    focusOnSelect: true,
-    centerPadding: "10px",
-    arrows: true,
-  };
-
+export default function Banner() {
   const slidesData = [
     {
       id: 1,
@@ -72,8 +42,6 @@ export default function ProductDetail(props) {
     },
   ];
 
-  // ######################
-
   const alwaysInPlace = true,
     overlayOpacity = 0.6,
     switchSides = false,
@@ -84,69 +52,131 @@ export default function ProductDetail(props) {
     fillGapTop = 10,
     fillGapBottom = 10;
 
-  const zoomImages = () => {
-    slidesData.map((slide) => <div style={{ maxWidth: "1000px" }}>AAA</div>);
-  };
-
   return (
-    <div className="App">
-      <div className="slider-wrapper">
-        {zoomImages()}
-        <Slider
-          {...settingsMain}
-          asNavFor={nav2}
-          ref={(slider) => setSlider1(slider)}
-        >
-          {slidesData.map((slide) => (
-            <div className="slick-slide" key={slide.id}>
-              <h2 className="slick-slide-title">{slide.title}</h2>
-              <div
-                style={{ maxWidth: "600px" }}
-                className="slick-slide"
-                key={slide.id}
-              >
-                <SideBySideMagnifier
-                  className="input-position"
-                  style={{ order: switchSides ? "1" : "0" }}
-                  imageSrc={`https://picsum.photos/1200/1300?img=${slide.id}`}
-                  largeImageSrc={`https://picsum.photos/1200/1300?img=${slide.id}`}
-                  alwaysInPlace={alwaysInPlace}
-                  overlayOpacity={overlayOpacity}
-                  switchSides={switchSides}
-                  zoomPosition="left"
-                  inPlaceMinBreakpoint={641}
-                  fillAvailableSpace={fillAvailableSpace}
-                  fillAlignTop={fillAlignTop}
-                  fillGapTop={fillGapTop}
-                  fillGapRight={fillGapRight}
-                  fillGapBottom={fillGapBottom}
-                  fillGapLeft={fillGapLeft}
-                  zoomContainerBorder="1px solid #ccc"
-                  zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
-                />
-              </div>
-              <label className="slick-slide-label">{slide.label}</label>
-            </div>
-          ))}
-        </Slider>
-        <div className="thumbnail-slider-wrap" style={{ maxWidth: "300px" }}>
-          <Slider
-            {...settingsThumbs}
-            asNavFor={nav1}
-            ref={(slider) => setSlider2(slider)}
-          >
-            {slidesData.map((slide) => (
-              <div className="slick-slide" key={slide.id}>
-                <img
-                  style={{ maxHeight: "100px" }}
-                  className="slick-slide-image"
-                  src={`https://picsum.photos/1200/1300?img=${slide.id}`}
-                />
-              </div>
-            ))}
-          </Slider>
+    <div className="product-detail-page-container">
+      <Carousel
+        autoPlay={false}
+        swipeable
+        emulateTouch
+        infiniteLoop
+        stopOnHover
+      >
+        {/* {slidesData.map((slide) => (
+          <div className="product-dtail-page-carousel-image-zoom">
+            <img
+              className="thumbnail-carousel-zoom"
+              src={`https://picsum.photos/1200/1300?img=${slide.id}`}
+            />
+            <SideBySideMagnifier
+              className="input-position"
+              style={{ order: switchSides ? "1" : "0" }}
+              imageSrc={`https://picsum.photos/1200/1300?img=${slide.id}`}
+              largeImageSrc={`https://picsum.photos/1200/1300?img=${slide.id}`}
+              alwaysInPlace={alwaysInPlace}
+              overlayOpacity={overlayOpacity}
+              switchSides={switchSides}
+              zoomPosition="left"
+              inPlaceMinBreakpoint={641}
+              fillAvailableSpace={fillAvailableSpace}
+              fillAlignTop={fillAlignTop}
+              fillGapTop={fillGapTop}
+              fillGapRight={fillGapRight}
+              fillGapBottom={fillGapBottom}
+              fillGapLeft={fillGapLeft}
+              zoomContainerBorder="1px solid #ccc"
+              zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
+            />
+          </div>
+        ))} */}
+        <div className="product-dtail-page-carousel-image-zoom">
+          <img className="thumbnail-carousel-zoom" src={img1} />
+          <SideBySideMagnifier
+            className="input-position"
+            style={{ order: switchSides ? "1" : "0" }}
+            imageSrc={img1}
+            largeImageSrc={img1}
+            alwaysInPlace={alwaysInPlace}
+            overlayOpacity={overlayOpacity}
+            switchSides={switchSides}
+            zoomPosition="left"
+            inPlaceMinBreakpoint={641}
+            fillAvailableSpace={fillAvailableSpace}
+            fillAlignTop={fillAlignTop}
+            fillGapTop={fillGapTop}
+            fillGapRight={fillGapRight}
+            fillGapBottom={fillGapBottom}
+            fillGapLeft={fillGapLeft}
+            zoomContainerBorder="1px solid #ccc"
+            zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
+          />
         </div>
-      </div>
+        <div className="product-dtail-page-carousel-image-zoom">
+          <img className="thumbnail-carousel-zoom" src={img2} />
+          <SideBySideMagnifier
+            className="input-position"
+            style={{ order: switchSides ? "1" : "0" }}
+            imageSrc={img2}
+            largeImageSrc={img2}
+            alwaysInPlace={alwaysInPlace}
+            overlayOpacity={overlayOpacity}
+            switchSides={switchSides}
+            zoomPosition="left"
+            inPlaceMinBreakpoint={641}
+            fillAvailableSpace={fillAvailableSpace}
+            fillAlignTop={fillAlignTop}
+            fillGapTop={fillGapTop}
+            fillGapRight={fillGapRight}
+            fillGapBottom={fillGapBottom}
+            fillGapLeft={fillGapLeft}
+            zoomContainerBorder="1px solid #ccc"
+            zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
+          />
+        </div>
+        <div className="product-dtail-page-carousel-image-zoom">
+          <img className="thumbnail-carousel-zoom" src={img3} />
+          <SideBySideMagnifier
+            className="input-position"
+            style={{ order: switchSides ? "1" : "0" }}
+            imageSrc={img3}
+            largeImageSrc={img3}
+            alwaysInPlace={alwaysInPlace}
+            overlayOpacity={overlayOpacity}
+            switchSides={switchSides}
+            zoomPosition="left"
+            inPlaceMinBreakpoint={641}
+            fillAvailableSpace={fillAvailableSpace}
+            fillAlignTop={fillAlignTop}
+            fillGapTop={fillGapTop}
+            fillGapRight={fillGapRight}
+            fillGapBottom={fillGapBottom}
+            fillGapLeft={fillGapLeft}
+            zoomContainerBorder="1px solid #ccc"
+            zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
+          />
+        </div>
+        <div className="product-dtail-page-carousel-image-zoom">
+          <img className="thumbnail-carousel-zoom" src={img4} />
+          <SideBySideMagnifier
+            className="input-position"
+            style={{ order: switchSides ? "1" : "0" }}
+            imageSrc={img4}
+            largeImageSrc={img4}
+            alwaysInPlace={alwaysInPlace}
+            overlayOpacity={overlayOpacity}
+            switchSides={switchSides}
+            zoomPosition="left"
+            inPlaceMinBreakpoint={641}
+            fillAvailableSpace={fillAvailableSpace}
+            fillAlignTop={fillAlignTop}
+            fillGapTop={fillGapTop}
+            fillGapRight={fillGapRight}
+            fillGapBottom={fillGapBottom}
+            fillGapLeft={fillGapLeft}
+            zoomContainerBorder="1px solid #ccc"
+            zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
+          />
+        </div>
+      </Carousel>
     </div>
   );
 }
